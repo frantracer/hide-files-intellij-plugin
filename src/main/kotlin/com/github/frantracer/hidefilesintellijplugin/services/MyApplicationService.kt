3ba027d18b3cfe500cd622ac93ccd00171d7e277
GitHub Action <action@github.com>
2022-01-11 19:36:27 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/frantracer/hidefilesintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.frantracer.hidefilesintellijplugin.services
+
+import com.github.frantracer.hidefilesintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
